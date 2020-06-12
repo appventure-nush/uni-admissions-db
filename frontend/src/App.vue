d8ea80659c6dece33260c69e3a801714170e9def
Lam Jun Rong <h1710074@nushigh.edu.sg>
2020-06-12 16:19:32 +0800

    Basic vue and vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-toolbar-title>
+        Hello, world
+      </v-toolbar-title>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    hello: "Hello, world",
+  }),
+};
+</script>
